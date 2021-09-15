fec0f8f7e27d05ce0d8c300d8cde89657c1923d7
Ron Newcomb <pscion@yahoo.com>
2021-09-14 19:34:54 -0700

    wip

--- src/character.ts
+++ src/character.ts
@@ -1,15 +1,21 @@
-import { damagePoints, meterPoints, I3DModel, IAudio, FrameData } from "./interfaces.js";
+import { damagePoints, meterPoints, I3DModel, IAudio, FrameData, pixelCount } from "./interfaces.js";
 
 export class Character {
-    isAirborne = false;
+    x: pixelCount = 0;
+    y: pixelCount = 0;
+    facingLeft: boolean = true;
+    isAirborne: boolean = false;
     health: damagePoints = 1000;
     meter: meterPoints = 0;
 
     constructor(public models: I3DModel[], public soundBites: IAudio[], public fdata: FrameData) {
-        this.reset();
+        this.reset(true);
     }
 
-    reset() {
+    reset(player1side: boolean) {
+        this.x = 500 + (player1side ? -1 : +1) * 100;
+        this.y = 0;
+        this.facingLeft = player1side;
         this.isAirborne = false;
         this.health = 1000;
         this.meter = 0;
